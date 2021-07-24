2a8c7965787d0ef948b218fbf367b9dd658c49b7
DungHoang <kathymaidung@gmail.com>
2021-07-24 22:26:08 +0300

    add greeting element to ComponentsScreen

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 
 export default function ComponentsScreen() {
-  return <Text style={styles.textStyle}>This is the components screen</Text>;
+  const greeting = <Text>Hello to you!</Text>;
+  return (
+    <View>
+      <Text style={styles.textStyle}>This is the components screen</Text>
+      {greeting}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
