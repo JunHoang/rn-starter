a0cb3d42cf4721b21c0f4e0654c0cdd6dde49907
DungHoang <kathymaidung@gmail.com>
2021-07-25 14:45:08 +0300

    add Counter Screen boilerplate

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function CounterScreen() {
+  return (
+    <View>
+      <Text>Current Count: </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
