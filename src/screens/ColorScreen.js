81585362148c2ca278c705d4b2d768c632e29207
DungHoang <kathymaidung@gmail.com>
2021-07-25 15:16:40 +0300

    add ColorScreen boilerplate

---
+++ src/screens/ColorScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function ColorScreen() {
+  return (
+    <View>
+      <Button title="Add a Color" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -28,6 +28,10 @@ const HomeScreen = ({ navigation }) => {
         onPress={() => navigation.navigate("Counter")}
         title="Go to Counter Demo"
       />
+      <Button
+        onPress={() => navigation.navigate("Color")}
+        title="Go to Color Demo"
+      />
     </View>
   );
 };
