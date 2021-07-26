7d4ae0fc63fdb6398d3174ce30fc6a9261bd88fd
DungHoang <kathymaidung@gmail.com>
2021-07-26 20:22:44 +0300

    flex direction

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,10 +3,24 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function BoxScreen() {
   return (
-    <View>
-      <Text>Box Screem</Text>
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>Child #1</Text>
+      <Text style={styles.textStyle}>Child #2</Text>
+      <Text style={styles.textStyle}>Child #3</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: "black",
+    height: 200,
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  textStyle: {
+    borderWidth: 3,
+    borderColor: "steelblue",
+  },
+});
