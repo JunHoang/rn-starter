e5f3459273ff1c817ee3a4cd85ac6a1b1cd43080
DungHoang <kathymaidung@gmail.com>
2021-07-26 17:44:05 +0300

    Add Text screen boilerplate

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function TextScreen() {
+  return (
+    <View>
+      <Text>Text Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
