5cbcf4a39f501b6e4f0b104415699f4181a60660
DungHoang <kathymaidung@gmail.com>
2021-07-26 20:05:54 +0300

    add BoxScreen boilerplate

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function BoxScreen() {
+  return (
+    <View>
+      <Text>Box Screem</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
