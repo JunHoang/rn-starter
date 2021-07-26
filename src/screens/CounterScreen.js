adf5e132284bf7be098d8513fd93a7bf3148c3de
DungHoang <kathymaidung@gmail.com>
2021-07-26 15:04:53 +0300

    Change Counter state by using Router

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,14 +1,39 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 
+const reducer = (state, action) => {
+  //state === {count: number}
+  //action === {type: 'increase' || 'decrease', payload: 1 | -1}
+
+  switch (action.type) {
+    case "increase":
+      return { ...state, count: state.count + action.payload };
+    case "decrease":
+      return { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
 export default function CounterScreen() {
-  const [counter, setCounter] = useState(0);
+  initialState = { count: 0 };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count } = state;
 
   return (
     <View>
-      <Button title="Increase" onPress={() => setCounter(counter + 1)} />
-      <Button title="Decrease" onPress={() => setCounter(counter - 1)} />
-      <Text>Current Count: {counter}</Text>
+      <Button
+        title="Increase"
+        onPress={() => {
+          dispatch({ type: "increase", payload: 1 });
+        }}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => {
+          dispatch({ type: "decrease", payload: 1 });
+        }}
+      />
+      <Text>Current Count: {count}</Text>
     </View>
   );
 }
